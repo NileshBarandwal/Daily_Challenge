3a5e42de365e64b9be02f7af8e04342b7f1bef20
NileshBarandwal <nbarandwal@gmail.com>
2024-05-20 13:30:56 +0530

    LeetCode: 197. Rising Temperature || Added SQL Schema || Database || MySQL

--- LeetCode/Database/RisingTemperature.sql
+++ LeetCode/Database/RisingTemperature.sql
@@ -1,3 +1,13 @@
+/*
+# 197. Rising Temperature
+## SQL Schema
+Create table If Not Exists Weather (id int, recordDate date, temperature int)
+Truncate table Weather
+insert into Weather (id, recordDate, temperature) values ('1', '2015-01-01', '10')
+insert into Weather (id, recordDate, temperature) values ('2', '2015-01-02', '25')
+insert into Weather (id, recordDate, temperature) values ('3', '2015-01-03', '20')
+insert into Weather (id, recordDate, temperature) values ('4', '2015-01-04', '30')
+*/
 # Write your MySQL query statement below
 SELECT w1.id
 FROM Weather w1
