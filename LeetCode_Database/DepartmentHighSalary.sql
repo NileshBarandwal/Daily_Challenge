3c75da47f33048ca15ec116f4b69f3083b3d01ae
NileshBarandwal <nbarandwal@gmail.com>
2024-05-31 16:16:20 +0530

    LeetCode: 184. Department High Salary || C++

---
+++ LeetCode_Database/DepartmentHighSalary.sql
@@ -0,0 +1,31 @@
+/*
+# 184. Department Highest Salary
+## SQL Schema
+
+Create table If Not Exists Employee (id int, name varchar(255), salary int, departmentId int)
+Create table If Not Exists Department (id int, name varchar(255))
+Truncate table Employee
+insert into Employee (id, name, salary, departmentId) values ('1', 'Joe', '70000', '1')
+insert into Employee (id, name, salary, departmentId) values ('2', 'Jim', '90000', '1')
+insert into Employee (id, name, salary, departmentId) values ('3', 'Henry', '80000', '2')
+insert into Employee (id, name, salary, departmentId) values ('4', 'Sam', '60000', '2')
+insert into Employee (id, name, salary, departmentId) values ('5', 'Max', '90000', '1')
+Truncate table Department
+insert into Department (id, name) values ('1', 'IT')
+insert into Department (id, name) values ('2', 'Sales')
+
+*/
+
+# Write your MySQL query statement below
+
+select d.name as Department, e.name as Employee, e.salary as Salary
+from Employee e
+join Department d
+on e.departmentId = d.id
+where (
+    select count(distinct salary)
+    from Employee
+    where departmentId = e.departmentId and salary > e.salary
+) < 1
+-- order by
+-- d.name, e.salary desc;
