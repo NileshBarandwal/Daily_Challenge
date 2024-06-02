f8028913d12f294fa26c506cd568eb6560b6e88b
NileshBarandwal <nbarandwal@gmail.com>
2024-06-02 14:48:32 +0530

    June||Day2||LeetCode :344.Reverse String|| C code || Daily Challenge

---
+++ LeetCode Monthly Challenge/June/Day 2/ReverseString.c
@@ -0,0 +1,9 @@
+# C code
+
+void reverseString(char* s, int sSize) {
+    for(int i = 0; i<sSize/2; i++){
+        int temp = s[i];
+        s[i] = s[sSize - i - 1 ];
+        s[sSize - i - 1] = temp;
+    }
+}
