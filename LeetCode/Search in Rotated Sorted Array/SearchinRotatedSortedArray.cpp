b3e7817a7d5c90341265c8c80bac38a0c7fb9958
NileshBarandwal <nbarandwal@gmail.com>
2024-06-19 20:12:32 +0530

    LeetCode: 33. Search in Rotated Sorted Array || Daily Challenge || C++

---
+++ LeetCode/Search in Rotated Sorted Array/SearchinRotatedSortedArray.cpp
@@ -0,0 +1,37 @@
+// 33. Search in Rotated Sorted Array
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int start = 0;
+        int end = nums.size() - 1;
+	
+	//Binary Search - Perform on both side Right and Left
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            // Left half is sorted
+            if (nums[start] <= nums[mid]) {
+                if (nums[start] <= target && target < nums[mid]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            }
+            // Right half is sorted
+            else {
+                if (nums[mid] < target && target <= nums[end]) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+};
