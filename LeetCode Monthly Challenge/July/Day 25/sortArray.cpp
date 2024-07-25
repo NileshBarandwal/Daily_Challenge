d44bc26b851654728e9ab893e0a110aa08b332f9
NileshBarandwal <nbarandwal@gmail.com>
2024-07-25 22:23:42 +0530

    July || Day 25 || LeetCode: 912. Sort an Array || Daily Challenge || C++

---
+++ LeetCode Monthly Challenge/July/Day 25/sortArray.cpp
@@ -0,0 +1,49 @@
+// 912. Sort an Array
+
+class Solution {
+public:
+    vector<int> sortArray(vector<int>& nums) {
+        heapSort(nums);
+        return nums;
+    }
+    
+    void heapSort(vector<int>& nums) {
+        int n = nums.size();
+        
+        // Build heap (rearrange array)
+        for (int i = n / 2 - 1; i >= 0; i--)
+            heapify(nums, n, i);
+        
+        // One by one extract an element from heap
+        for (int i = n - 1; i > 0; i--) {
+            // Move current root to end
+            swap(nums[0], nums[i]);
+            
+            // call max heapify on the reduced heap
+            heapify(nums, i, 0);
+        }
+    }
+    
+    // To heapify a subtree rooted with node i which is an index in nums[]
+    void heapify(vector<int>& nums, int n, int i) {
+        int largest = i; // Initialize largest as root
+        int left = 2 * i + 1; // left = 2*i + 1
+        int right = 2 * i + 2; // right = 2*i + 2
+        
+        // If left child is larger than root
+        if (left < n && nums[left] > nums[largest])
+            largest = left;
+        
+        // If right child is larger than largest so far
+        if (right < n && nums[right] > nums[largest])
+            largest = right;
+        
+        // If largest is not root
+        if (largest != i) {
+            swap(nums[i], nums[largest]);
+            
+            // Recursively heapify the affected sub-tree
+            heapify(nums, n, largest);
+        }
+    }
+};
